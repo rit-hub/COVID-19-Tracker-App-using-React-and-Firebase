ddf3cb8a6694947a70ee3b5eee109feacde6f54c
rit-hub <72191249+rit-hub@users.noreply.github.com>
2021-06-22 21:39:41 +0530

    initial commit

--- covid-app/covid-19-tracker/src/App.js
+++ covid-app/covid-19-tracker/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'
+import{MenuItem, FormControl, Select} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM naming convention
+    <div className="app"> 
+      <div className="app__header">
+        <h1>hi</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value ="abc">
+            <MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldwide">abc1</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      
+      {/*Header Component*/}
+      {/*Title + Select input Dropdown component*/}
+      {/*Infoboxes Component*/}
+      {/*Infoboxes Component*/}
+      {/*Infoboxes Component*/}
+
+       {/*Table Component*/}
+        {/*graph Component*/}
+      
+       {/*Map Component*/}
+
+      
     </div>
   );
 }
