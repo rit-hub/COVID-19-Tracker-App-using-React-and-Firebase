cfc305eab3f859a9da9828b522c451b148a58c88
rit-hub <72191249+rit-hub@users.noreply.github.com>
2021-06-23 19:12:44 +0530

    countries added

--- covid-app/covid-19-tracker/src/App.js
+++ covid-app/covid-19-tracker/src/App.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import{MenuItem, FormControl, Select} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const [countries,setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  useEffect(() => {
+    const getCountriesData = async ()=> {
+      await fetch("https://disease.sh/v3/covid-19/countries").then((response)=> response.json()).then((data) => {
+        const countries = data.map((country) =>(
+          {
+            name: country.country, // United Kingdom, United States,India
+            value: country.countryInfo.iso2, // UK,USA, IN
+          }
+        ));
+        setCountries(countries);
+      });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    // console.log(countryCode);
+    setCountry(countryCode);
+  }
   return (
     //BEM naming convention
     <div className="app"> 
       <div className="app__header">
-        <h1>hi</h1>
+        <h1>Covid-19 Tracker</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value ="abc">
-            <MenuItem value="worldwide">worldwide</MenuItem>
-            <MenuItem value="worldwide">abc1</MenuItem>
+          <Select variant="outlined" value = {country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {
+              countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
           </Select>
         </FormControl>
       </div>
